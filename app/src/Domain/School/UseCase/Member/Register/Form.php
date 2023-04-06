cedccc801b4e11452e9a88a685415a0fc69202f4
Anna Borzenko <annnechko@gmail.com>
2023-04-06 23:24:45 +0300

    fix, hide plain password in form

--- app/src/Domain/School/UseCase/Member/Register/Form.php
+++ app/src/Domain/School/UseCase/Member/Register/Form.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\School\UseCase\Member\Register;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class Form extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('plainPassword');
+            ->add('plainPassword', PasswordType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
