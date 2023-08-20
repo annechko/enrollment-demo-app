ae348c5140983ea9e36375c2ec44336015e1f726
Anna Borzenko <annnechko@gmail.com>
2023-08-20 14:46:50 +0300

    ui, delete unused header items

--- app/assets/App/Common/AppHeader.js
+++ app/assets/App/Common/AppHeader.js
@@ -1,15 +1,7 @@
 import {
-  cilBell,
-  cilEnvelopeOpen,
-  cilList
-} from '@coreui/icons'
-import CIcon from '@coreui/icons-react'
-import {
   CContainer,
   CHeader,
   CHeaderNav,
-  CNavItem,
-  CNavLink,
 } from '@coreui/react'
 import React, {useContext} from 'react'
 import {AppHeaderDropdown} from "./index";
@@ -29,23 +21,6 @@ const AppHeader = () => {
     <CHeader position="sticky" className="mb-2">
       <CContainer fluid className="header-items-right">
         {sectionName}
-        <CHeaderNav>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilBell} size="lg"/>
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilList} size="lg"/>
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilEnvelopeOpen} size="lg"/>
-            </CNavLink>
-          </CNavItem>
-        </CHeaderNav>
         <CHeaderNav className="ms-3">
           <AppHeaderDropdown/>
         </CHeaderNav>
--- app/assets/App/Common/AppHeaderDropdown.js
+++ app/assets/App/Common/AppHeaderDropdown.js
@@ -1,16 +1,10 @@
 import {
   cilAccountLogout,
-  cilBell,
-  cilCommentSquare,
-  cilEnvelopeOpen,
-  cilSettings,
-  cilTask,
   cilUser,
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 import {CssHelper} from "../Helper/CssHelper";
 import {
-  CBadge,
   CDropdown,
   CDropdownDivider,
   CDropdownHeader,
@@ -58,43 +52,10 @@ const AppHeaderDropdown = () => {
 
         <OtherProfiles></OtherProfiles>
 
-        <CDropdownHeader className="bg-light fw-semibold py-2">Activity</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilBell} className="me-2"/>
-          Updates
-          <CBadge color="info" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilEnvelopeOpen} className="me-2"/>
-          Messages
-          <CBadge color="success" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilTask} className="me-2"/>
-          Tasks
-          <CBadge color="danger" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilCommentSquare} className="me-2"/>
-          Comments
-          <CBadge color="warning" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
         <CDropdownHeader className="bg-light fw-semibold py-2">Settings</CDropdownHeader>
         <CDropdownItem href="#">
           <CIcon icon={cilUser} className="me-2"/>
           Profile
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilSettings} className="me-2"/>
-          Settings
         </CDropdownItem>
         <CDropdownDivider/>
         <CDropdownItem href={window.abeApp.urls.logout}>
