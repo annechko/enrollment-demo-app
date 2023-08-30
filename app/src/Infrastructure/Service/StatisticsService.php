d97bd373172a72750d44f3605b7da403e208f880
Anna Borzenko <annnechko@gmail.com>
2023-08-30 17:10:51 +0300

    refactor to delete duplicated logic

--- app/src/Infrastructure/Service/StatisticsService.php
+++ app/src/Infrastructure/Service/StatisticsService.php
@@ -10,6 +10,13 @@ use App\ReadModel\Admin\Statistics\StatsFetcher;
 
 class StatisticsService
 {
+    private const DATE_STRING_YEAR_AGO = 'first day of this month last year';
+    private const FORMAT_MONTH = 'M y';
+    private const INTERVAL_MONTH = 'P1M';
+    private const FORMAT_DAY = 'j M';
+    private const INTERVAL_DAY = 'P1D';
+    private const DATE_STRING_MONTH_AGO = '30 days ago';
+
     public function __construct(
         private readonly StatsFetcher $fetcher,
     ) {
@@ -18,21 +25,48 @@ class StatisticsService
     public function getByType(Filter $filter): StatisticsResponseDto
     {
         return match ($filter->type->value) {
-            ReportTypeEnum::SCHOOL_REGISTRATIONS_YEAR->value => $this->getRegistrationsPerYear(),
-            ReportTypeEnum::SCHOOL_REGISTRATIONS_MONTH->value => $this->getRegistrationsPerMonth(),
-            ReportTypeEnum::STUDENT_APPLICATIONS_MONTH->value => $this->getStudentApplicationsPerMonth(
+            ReportTypeEnum::SCHOOL_REGISTRATIONS_YEAR->value => $this->getData(
+                self::DATE_STRING_YEAR_AGO,
+                $this->fetcher->fetchSchoolRegistrationsByMonth(...),
+                self::FORMAT_MONTH,
+                self::INTERVAL_MONTH,
+                'y-m'
             ),
-            ReportTypeEnum::STUDENT_APPLICATIONS_YEAR->value => $this->getStudentApplicationsPerYear(
+            ReportTypeEnum::SCHOOL_REGISTRATIONS_MONTH->value => $this->getData(
+                self::DATE_STRING_MONTH_AGO,
+                $this->fetcher->fetchSchoolRegistrationsByDay(...),
+                self::FORMAT_DAY,
+                self::INTERVAL_DAY,
+                'm-d'
+            ),
+            ReportTypeEnum::STUDENT_APPLICATIONS_YEAR->value => $this->getData(
+                self::DATE_STRING_YEAR_AGO,
+                $this->fetcher->fetchStudentApplicationsByMonth(...),
+                self::FORMAT_MONTH,
+                self::INTERVAL_MONTH,
+                'y-m'
+            ),
+            ReportTypeEnum::STUDENT_APPLICATIONS_MONTH->value => $this->getData(
+                self::DATE_STRING_MONTH_AGO,
+                $this->fetcher->fetchStudentApplicationsByDay(...),
+                self::FORMAT_DAY,
+                self::INTERVAL_DAY,
+                'm-d'
             ),
             default => throw new \DomainException('Unknown report type.')
         };
     }
 
-    private function getRegistrationsPerYear(): StatisticsResponseDto
-    {
-        $yearAgo = (new \DateTimeImmutable('first day of this month last year'))->setTime(0, 0, 0);
+    private function getData(
+        string $dateString,
+        callable $fetcherCallback,
+        string $labelFormat,
+        string $step,
+        string $fetcherDateFormat,
+    ): StatisticsResponseDto {
+        $yearAgo = (new \DateTimeImmutable($dateString))->setTime(0, 0, 0);
         $now = new \DateTimeImmutable();
-        $dateToAmountResults = $this->fetcher->fetchSchoolRegistrationsByMonth(
+        $dateToAmountResults = $fetcherCallback(
             $yearAgo
         );
         $datePointer = clone $yearAgo;
@@ -40,104 +74,15 @@ class StatisticsService
         $data = [];
 
         while ($datePointer < $now) {
-            $curMonth = $datePointer->format('y-m');
-            $labels[$curMonth] = $datePointer->format('M y');
-            $datePointer = $datePointer->add(new \DateInterval('P1M'));
+            $curMonth = $datePointer->format($fetcherDateFormat);
+            $labels[$curMonth] = $datePointer->format($labelFormat);
+            $datePointer = $datePointer->add(new \DateInterval($step));
         }
         foreach ($labels as $label => $date) {
             $data[] = $dateToAmountResults[$label] ?? 0;
         }
 
         $maxY = intval(max($data) + round(max($data)) / 2);
-
-        return new StatisticsResponseDto(
-            array_values($labels),
-            $data,
-            $maxY
-        );
-    }
-
-    private function getRegistrationsPerMonth(): StatisticsResponseDto
-    {
-        $sinceDate = (new \DateTimeImmutable())->sub(new \DateInterval('P1M'))->setTime(0, 0, 0);
-        $now = new \DateTimeImmutable();
-        $dateToAmountResults = $this->fetcher->fetchSchoolRegistrationsByDay(
-            $sinceDate
-        );
-        $datePointer = clone $sinceDate;
-        $labels = [];
-        $data = [];
-
-        while ($datePointer < $now) {
-            $index = $datePointer->format('m-d');
-            $labels[$index] = $datePointer->format('j M');
-            $datePointer = $datePointer->add(new \DateInterval('P1D'));
-        }
-
-        foreach ($labels as $label => $date) {
-            $data[] = $dateToAmountResults[$label] ?? 0;
-        }
-
-        $maxY = (int) round(max($data) + round(max($data)) / 2);
-
-        return new StatisticsResponseDto(
-            array_values($labels),
-            $data,
-            $maxY
-        );
-    }
-
-    private function getStudentApplicationsPerYear(): StatisticsResponseDto
-    {
-        $yearAgo = (new \DateTimeImmutable('first day of this month last year'))->setTime(0, 0, 0);
-        $now = new \DateTimeImmutable();
-        $dateToAmountResults = $this->fetcher->fetchStudentApplicationsByMonth(
-            $yearAgo
-        );
-        $datePointer = clone $yearAgo;
-        $labels = [];
-        $data = [];
-
-        while ($datePointer < $now) {
-            $curMonth = $datePointer->format('y-m');
-            $labels[$curMonth] = $datePointer->format('M y');
-            $datePointer = $datePointer->add(new \DateInterval('P1M'));
-        }
-        foreach ($labels as $label => $date) {
-            $data[] = $dateToAmountResults[$label] ?? 0;
-        }
-
-        $maxY = intval(max($data) + round(max($data)) / 2);
-
-        return new StatisticsResponseDto(
-            array_values($labels),
-            $data,
-            $maxY
-        );
-    }
-
-    private function getStudentApplicationsPerMonth(): StatisticsResponseDto
-    {
-        $sinceDate = (new \DateTimeImmutable())->sub(new \DateInterval('P1M'))->setTime(0, 0, 0);
-        $now = new \DateTimeImmutable();
-        $dateToAmountResults = $this->fetcher->fetchStudentApplicationsByDay(
-            $sinceDate
-        );
-        $datePointer = clone $sinceDate;
-        $labels = [];
-        $data = [];
-
-        while ($datePointer < $now) {
-            $index = $datePointer->format('m-d');
-            $labels[$index] = $datePointer->format('j M');
-            $datePointer = $datePointer->add(new \DateInterval('P1D'));
-        }
-
-        foreach ($labels as $label => $date) {
-            $data[] = $dateToAmountResults[$label] ?? 0;
-        }
-
-        $maxY = (int) round(max($data) + round(max($data)) / 2);
 
         return new StatisticsResponseDto(
             array_values($labels),
