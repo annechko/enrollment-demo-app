a581eba5dd918f86ad361ed212f8a4569a8e517d
Anna Borzenko <annnechko@gmail.com>
2023-08-31 18:33:07 +0300

    remove unused code

--- app/assets/App/Common/index.js
+++ app/assets/App/Common/index.js
@@ -1,11 +1,9 @@
-import AppBreadcrumb from './AppBreadcrumb'
 import AppFooter from './AppFooter'
 import AppHeader from './AppHeader'
 import AppHeaderDropdown from './AppHeaderDropdown'
 import AppSidebar from './AppSidebar'
 
 export {
-  AppBreadcrumb,
   AppFooter,
   AppHeader,
   AppHeaderDropdown,
